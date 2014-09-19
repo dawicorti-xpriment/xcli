c9aa43c869fd559862d9f407da12841717299e1f
David Corticchiato <david.corticchiato@gmail.com>
2014-09-19 07:00:52 +0200

    xcli bin

--- test/generator.test.js
+++ test/generator.test.js
@@ -1,9 +1,14 @@
 var assert = require('assert');
+var fs = require('fs');
 var generator = require('../lib/generator');
 var streamify = require('streamify');
-
+var rimraf = require('rimraf');
 
 describe('generator', function () {
+
+  afterEach(function (done) {
+    rimraf(__dirname + '/ios-project', done);
+  });
 
   it('should send file stats when reading top folder', function (done) {
     var dirContent = [];
@@ -18,7 +23,23 @@ describe('generator', function () {
   });
 
   it('should copy tree', function (done) {
-    generator.createFromSkeleton('ios-empty', __dirname + '/fixtures2', null, done);
+    generator.createFromSkeleton('ios-empty', __dirname + '/ios-project', null, function () {
+      fs.exists(__dirname + '/ios-project', function (result) {
+        assert.equal(true, result);
+        done();
+      });
+    });
+  });
+
+  it('should replace folders names', function (done) {
+    var project = {productName: 'foobar'}
+
+    generator.createFromSkeleton('ios-empty', __dirname + '/ios-project', project, function () {
+      fs.exists(__dirname + '/ios-project/foobar', function (result) {
+        assert.equal(true, result);
+        done();
+      });
+    });
   });
 
 });
